7aad4588852018989c60fa591eb11921f9f317e6
theSoenke <theSoenke@users.noreply.github.com>
2015-10-29 17:12:17 +0100

    Update job_script
    slurm datei wird nicht mehr erzeugt und auf jedem der 4 knoten laufen 4 prozesse

--- uebung3/job_script.sh
+++ uebung3/job_script.sh
@@ -1,5 +1,8 @@
-#! /bin/bash
-#SBATCH -A froehlich
-#SBATCH -N 4
-#SBATCH -n 4
-srun ./timescript.sh > ./timescript.out
+#!/bin/bash
+#SBATCH -N 4 --ntasks-per-node=4
+#SBATCH -n 16
+#SBATCH -o timescript.out
+
+srun ./timescript.sh
+
+echo fertig > job_script.out
