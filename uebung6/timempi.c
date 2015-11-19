78c896ed2c5ba13eed0532a6c3400e66bfeed6d1
Arne Westphal <3westpha@informatik.uni-hamburg.de>
2015-11-19 23:57:58 +0100

    Fehler behoben. Reduce hinzugefuegt (A2). getestet mit srun ./mpi_job

--- uebung6/timempi.c
+++ uebung6/timempi.c
@@ -26,30 +26,35 @@ int main(int argc, char** argv)
 	{	
 		buffer = malloc(sizeof(char) * length * num_processes);
 	}
-	else
-	{
-		struct timeval tv;
-		gettimeofday(&tv, NULL);
-		
-		char time[40];
-		strftime(time, 40, "%Y-%m-%d %T", localtime(&tv.tv_sec));
-		char hostname[200];
-		gethostname(hostname, sizeof(hostname));
-		snprintf(output, sizeof(output), "%s: %s.%ld", hostname, time, tv.tv_sec);
-	}
+	
+	
+	// Get Hostname and time
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	
+	char time[40];
+	strftime(time, 40, "%Y-%m-%d %T", localtime(&tv.tv_sec));
+	char hostname[200];
+	gethostname(hostname, sizeof(hostname));
+	snprintf(output, sizeof(output), "%s: %s.%ld", hostname, time, tv.tv_usec);
 
-	MPI_Gather(&output, length, MPI_CHAR, buffer, length, MPI_CHAR, 0, MPI_COMM_WORLD);
-
-	if(rank == 0 && num_processes > 1)
+    // Gather hostname and time into buffer
+	MPI_Gather(&output, length, MPI_CHAR, 
+	            buffer, length, MPI_CHAR, 0, MPI_COMM_WORLD);
+	            
+    int min_micro;
+    MPI_Reduce(&tv.tv_usec, &min_micro, 1, MPI_INT, MPI_MIN,
+	       0, MPI_COMM_WORLD);
+	       
+	if(rank == 0 && num_processes >= 1)
 	{
 		for(int i = 0; i < num_processes; i++)
 		{
 			printf("%s\n", buffer + (length * i));
 		}
+		printf("%d\n", min_micro);
 	}
 
-
-	
 	MPI_Barrier(MPI_COMM_WORLD);
 
 	printf("Rang %d beendet jetzt!\n", rank);
