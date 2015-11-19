b8851980bb955b2f5c2099521db464ce098fe631
theSoenke <theSoenke@users.noreply.github.com>
2015-11-19 19:42:08 +0100

    Mit timempi begonnen

---
+++ uebung6/timempi.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <time.h>
+#include <mpi.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main(int argc, char** argv)
+{
+	int length = 400;
+
+	MPI_Init(&argc, &argv);
+
+	int num_processes;
+	MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
+
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+
+	char output[length];
+	char *buffer;
+
+	if(rank == 0)
+	{	
+		buffer = malloc(sizeof(char) * length * num_processes);
+	}
+	else
+	{
+		struct timeval tv;
+		gettimeofday(&tv, NULL);
+		
+		char time[40];
+		strftime(time, 40, "%Y-%m-%d %T", localtime(&tv.tv_sec));
+		char hostname[200];
+		gethostname(hostname, sizeof(hostname));
+		snprintf(output, sizeof(output), "%s: %s.%ld", hostname, time, tv.tv_sec);
+	}
+
+	MPI_Gather(&output, length, MPI_CHAR, buffer, length, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+	if(rank == 0 && num_processes > 1)
+	{
+		for(int i = 0; i < num_processes; i++)
+		{
+			printf("%s\n", buffer + (length * i));
+		}
+	}
+
+
+	
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	printf("Rang %d beendet jetzt!\n", rank);
+
+	MPI_Finalize();
+	return 0;
+}
