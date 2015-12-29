139ecbd9cc6a0bf9baf16ba944a77562c0ccf83e
Arne Westphal <3westpha@informatik.uni-hamburg.de>
2015-12-29 02:00:49 +0100

    BSend eingebaut. Mit mpi_buffer_attach. Allerdings unsauber.

--- uebung9/pde/partdiff-par.c
+++ uebung9/pde/partdiff-par.c
@@ -256,7 +256,7 @@ shareValues(struct calculation_arguments const* arguments, struct options const*
 /* ************************************************************************ */
 static
 void
-recvValues(struct calculation_arguments const* arguments, struct options const* options, int m_in, int iteration) {
+recvValues(struct calculation_arguments const* arguments, struct options const* options, int m_in, int iteration, void* buffer, int buffer_size) {
     int const elements = 8 * options->interlines + 9;
 
     MPI_Status status;
@@ -276,6 +276,8 @@ recvValues(struct calculation_arguments const* arguments, struct options const* 
     if(rank_aft < num_procs && iteration > 0) {
         MPI_Recv(Matrix[m_size + 1], elements, MPI_DOUBLE, rank_aft, iteration, MPI_COMM_WORLD, &status);
     }
+
+    MPI_Buffer_detach(buffer, &buffer_size);
 }
 
 /* ************************************************************************ */
@@ -284,7 +286,7 @@ recvValues(struct calculation_arguments const* arguments, struct options const* 
 /* ************************************************************************ */
 static
 void
-sendValues(struct calculation_arguments const* arguments, struct options const* options, int m_in, int iteration) {
+sendValues(struct calculation_arguments const* arguments, struct options const* options, int m_in, int iteration, void * buffer, int buffer_size) {
     int const elements = 8 * options->interlines + 9;
 
     int rank_bef, rank_aft;
@@ -293,18 +295,21 @@ sendValues(struct calculation_arguments const* arguments, struct options const* 
 
     double** Matrix = arguments->Matrix[m_in];
 
+
+    MPI_Buffer_attach(buffer, buffer_size);
+
     if(rank_bef >= 0) {
-        MPI_Request request;
-        //MPI_Bsend(Matrix[1], elements, MPI_DOUBLE, rank_bef, iteration, MPI_COMM_WORLD);
-        MPI_Isend(Matrix[1], elements, MPI_DOUBLE, rank_bef, iteration+1, MPI_COMM_WORLD, &request);
-        MPI_Request_free(&request);
+        //MPI_Request request;
+        MPI_Bsend(Matrix[1], elements, MPI_DOUBLE, rank_bef, iteration+1, MPI_COMM_WORLD);
+        /* MPI_Isend(Matrix[1], elements, MPI_DOUBLE, rank_bef, iteration+1, MPI_COMM_WORLD, &request);
+        MPI_Request_free(&request); */
     }
 
     if(rank_aft < num_procs) {
-        MPI_Request request;
-        //MPI_Bsend(Matrix[m_size], elements, MPI_DOUBLE, rank_aft, iteration, MPI_COMM_WORLD);
-        MPI_Isend(Matrix[m_size], elements, MPI_DOUBLE, rank_aft, iteration, MPI_COMM_WORLD, &request);
-        MPI_Request_free(&request);
+        //MPI_Request request;
+        MPI_Bsend(Matrix[m_size], elements, MPI_DOUBLE, rank_aft, iteration, MPI_COMM_WORLD);
+        /* MPI_Isend(Matrix[m_size], elements, MPI_DOUBLE, rank_aft, iteration, MPI_COMM_WORLD, &request);
+        MPI_Request_free(&request); */
     }
 }
 
@@ -330,6 +335,9 @@ calculate (struct calculation_arguments const* arguments, struct calculation_res
     int iterations_nr = 0;
 
     int term_iteration = options->term_iteration;
+
+    void * mpi_buffer = (void *) allocateMemory((8 * options->interlines + 9) * 2 * sizeof(double));
+    int mpi_buffer_size = (8 * options->interlines + 9) * 2 * sizeof(double);
 
     /* Will be changed for precision, to finish the calculation */
     uint64_t local_termination = options->termination;
@@ -361,7 +369,7 @@ calculate (struct calculation_arguments const* arguments, struct calculation_res
         if (options->method == METH_GAUSS_SEIDEL)
     	{
             /* Empfangen der zusätzlichen zeilen */
-            recvValues(arguments, options, m1, iterations_nr);
+            recvValues(arguments, options, m1, iterations_nr, &mpi_buffer, mpi_buffer_size);
         }
 
         maxresiduum = 0;
@@ -407,13 +415,11 @@ calculate (struct calculation_arguments const* arguments, struct calculation_res
         else if (options->method == METH_GAUSS_SEIDEL)
     	{
             /* Senden der zusätzlichen zeilen */
-            sendValues(arguments, options, m1, iterations_nr);
+            sendValues(arguments, options, m1, iterations_nr, &mpi_buffer, mpi_buffer_size);
 
             /* Allreduce nur solange möglich */
             if(iterations_nr + rank >= num_procs) {
-                if((local_termination == TERM_ITER && rank < term_iteration)
-                    /* Der erste Prozess ist noch nicht fertig */
-                    || local_termination == TERM_PREC
+                if(local_termination == TERM_PREC
                     /* Es steht noch auf PREC also auch erster Rang nicht fertig. */)
                 {
                     MPI_Allreduce(MPI_IN_PLACE, &maxresiduum, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
